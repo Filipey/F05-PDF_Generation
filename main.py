def45f0e5468582d08e7d7b8644eb71774973d92
Filipe <filipeasm18@gmail.com>
2022-11-14 15:37:43 -0300

    Chore: Initial PDF export

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 from models.inspectionPdf import InspectionPdfDTO
 from PDF.service import generate_pdf
@@ -12,8 +13,10 @@ async def hello():
 
 @app.post("/pdf")
 async def get_inspection_report(pdfDto: InspectionPdfDTO):
-  generate_pdf(pdfDto)
-  return pdfDto
+  pdf = generate_pdf(pdfDto)
+
+  headers = {'Content-Disposition': f'attachment; filename={pdf}'}
+  return FileResponse(pdf, headers=headers, media_type="application/pdf")
 
 if __name__ == "__main__":
   uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
--- models/inspectionPdf.py
+++ models/inspectionPdf.py
@@ -5,8 +5,14 @@ from pydantic import BaseModel
 from models.pdfPhoto import PdfPhoto
 
 
+class Inspector(BaseModel):
+  name: str
+  role: str
+
+
 class InspectionPdfDTO(BaseModel):
   inspection_id: str
   local: str
   inspection_date: str
   content: List[PdfPhoto]
+  inspector: Inspector
